73212217776dfd2a2e9eaf5f2193862a53c9bb9c
David Tseluiko <david.tseluiko@gmail.com>
2025-12-06 19:51:15 -0600

    Make possible to switch between different projects

--- src/index.js
+++ src/index.js
@@ -2,7 +2,31 @@ import "./css/style.css";
 import { createNewProject } from "./createProject.js";
 
 const createProjectBtn = document.querySelector(".sidebar__button");
+const navList = document.querySelector(".nav__list");
 
 createProjectBtn.addEventListener("click", () => {
     createNewProject();
 });
+
+navList.addEventListener("click", (event) => {
+    const elementLastClass =
+        event.target.classList[event.target.classList.length - 1];
+
+    if (elementLastClass !== "nav__button--active") {
+        removeOldActiveClass();
+        event.target.classList.add("nav__button--active");
+    }
+});
+
+function removeOldActiveClass() {
+    const allNavBtns = Array.from(document.querySelectorAll(".nav__button"));
+
+    for (let button of allNavBtns) {
+        if (
+            button.classList[button.classList.length - 1] ===
+            "nav__button--active"
+        ) {
+            button.classList.remove("nav__button--active");
+        }
+    }
+}
