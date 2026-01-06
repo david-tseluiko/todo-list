3d517314fb771338d68383cb83470923f9a56f0e
David Tseluiko <david.tseluiko@gmail.com>
2026-01-06 13:17:12 -0600

    Create the basic file and DOM structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import "./css/style.css";
+
+const body = document.querySelector("body");
+
+(function createPrimaryGrid() {
+    const wrapper = document.createElement("div");
+    wrapper.className = "wrapper";
+
+    const sideBar = document.createElement("aside");
+    sideBar.className = "side";
+
+    const mainPage = document.createElement("main");
+    mainPage.className = "main";
+
+    wrapper.append(sideBar, mainPage);
+    body.appendChild(wrapper);
+})();
+
+(function updateSideBar() {
+    const sideBar = document.querySelector(".side");
+
+    const title = document.createElement("h1");
+    title.className = "side__title";
+    title.textContent = "Todo List";
+
+    const projectPanel = createProjectPanel()
+
+    sideBar.append(title, projectPanel[0], projectPanel[1]);
+})();
+
+function createProjectPanel() {
+    const addProject = document.createElement("button");
+    addProject.className = "side__add-button";
+    addProject.textContent = "Add Project";
+
+    const projects = document.createElement("div");
+    projects.className = "side__projects";
+
+    return [addProject, projects];
+}
